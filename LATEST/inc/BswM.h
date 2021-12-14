387e849e3763b10a42bdab86841f2b958bb91690
root <nagaraja.hm.0001@gmail.com>
2021-12-14 13:19:02 +0100

    Update: submodule BswM

---
+++ LATEST/inc/BswM.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class class_BswM{
+   public:
+      void InitFunction         (void);
+      void DeInitFunction       (void);
+      void CurrentWakeupSources (void);
+};
+
+extern class_BswM BswM;
+
---
+++ LATEST/src/BswM.cpp
@@ -0,0 +1,18 @@
+#include "BswM.h"
+
+#include "EcuM.h"
+
+class_BswM BswM;
+
+void class_BswM::InitFunction(void){
+}
+
+void class_BswM::DeInitFunction(void){
+}
+
+// EcuM.SelectShutdownTarget(...);
+// EcuM.GoDownHaltPoll(...);
+
+void class_BswM::CurrentWakeupSources(void){
+}
+
