bc499cb2159a1af8c2150cd9ae9216e79d385821
root <nagaraja.hm.0001@gmail.com>
2021-12-18 22:17:33 +0100

    Update: submodule BswM

--- LATEST/inc/BswM.h
+++ LATEST/inc/BswM.h
@@ -2,9 +2,31 @@
 
 class class_BswM{
    public:
-      void InitFunction         (void);
-      void DeInitFunction       (void);
-      void CurrentWakeupSources (void);
+      void InitFunction                   (void);
+      void DeInitFunction                 (void);
+      void CurrentWakeupSources           (void);
+      void PartitionRestarted             (void);
+      void CurrentIcomConfiguration       (void);
+      void CurrentState                   (void);
+      void CurrentMode                    (void);
+      void CurrentPNCMode                 (void);
+      void InitiateReset                  (void);
+      void ApplicationUpdated             (void);
+      void CurrentWakeup                  (void);
+      void PortGroupLinkStateChg          (void);
+      void GetVersionInfo                 (void);
+      void BroadcastStatus                (void);
+      void StateChangeNotification        (void);
+      void CurrentSchedule                (void);
+      void RequestMode                    (void);
+      void CarWakeUpIndication            (void);
+      void CurrentBlockMode               (void);
+      void CurrentJobMode                 (void);
+      void ClientServiceCurrentState      (void);
+      void ConsumedEventGroupCurrentState (void);
+      void EventHandlerCurrentState       (void);
+      void RequestPartitionReset          (void);
+      void MainFunction                   (void);
 };
 
 extern class_BswM BswM;
--- LATEST/src/BswM.cpp
+++ LATEST/src/BswM.cpp
@@ -16,3 +16,26 @@ void class_BswM::DeInitFunction(void){
 void class_BswM::CurrentWakeupSources(void){
 }
 
+void PartitionRestarted(void){}
+void CurrentIcomConfiguration(void){}
+void CurrentState(void){}
+void CurrentMode(void){}
+void CurrentPNCMode(void){}
+void InitiateReset(void){}
+void ApplicationUpdated(void){}
+void CurrentWakeup(void){}
+void PortGroupLinkStateChg(void){}
+void GetVersionInfo(void){}
+void BroadcastStatus(void){}
+void StateChangeNotification(void){}
+void CurrentSchedule(void){}
+void RequestMode(void){}
+void CarWakeUpIndication(void){}
+void CurrentBlockMode(void){}
+void CurrentJobMode(void){}
+void ClientServiceCurrentState(void){}
+void ConsumedEventGroupCurrentState(void){}
+void EventHandlerCurrentState(void){}
+void RequestPartitionReset(void){}
+void MainFunction(void){}
+
