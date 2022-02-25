153a7957ce1e00f7ac384cf37c1caa152a806653
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:07:46 +0100

    Update: submodule BswM

--- LATEST/BswM_EcuM.h
+++ LATEST/BswM_EcuM.h
@@ -27,8 +27,8 @@ class interface_BswM_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, BSWM_CODE) InitFunction   (void);
-      FUNC(void, BSWM_CODE) DeInitFunction (void);
+      virtual FUNC(void, BSWM_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, BSWM_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
