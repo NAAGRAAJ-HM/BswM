20479bd046d7786bc69882fc32a22c996e5446c3
root <nagaraja.hm.0001@gmail.com>
2022-03-30 12:42:50 +0200

    Update: AUTOSAR basic software service module BswM

--- LATEST/BswM.cpp
+++ LATEST/BswM.cpp
@@ -6,7 +6,7 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
-#include "module.hpp"
+#include "Module.hpp"
 #include "infBswM_EcuM.hpp"
 #include "infBswM_Dcm.hpp"
 #include "infBswM_SchM.hpp"
@@ -37,6 +37,9 @@ class module_BswM:
    public:
       module_BswM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
+      FUNC(void, _CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      );
       FUNC(void, BSWM_CODE) InitFunction   (void);
       FUNC(void, BSWM_CODE) DeInitFunction (void);
       FUNC(void, BSWM_CODE) MainFunction   (void);
@@ -73,7 +76,19 @@ VAR(module_BswM, BSWM_VAR) BswM(
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
-FUNC(void, BSWM_CODE) module_BswM::InitFunction(void){
+FUNC(void, BSWM_CODE) module_BswM::InitFunction(
+   CONSTP2CONST(CfgBswM_Type, CFGBSWM_CONFIG_DATA, CFGBSWM_APPL_CONST) lptrCfgBswM
+){
+   if(NULL_PTR == lptrCfgBswM){
+#if(STD_ON == BswM_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+// check lptrCfgBswM for memory faults
+// use PBcfg_BswM as back-up configuration
+   }
    BswM.IsInitDone = E_OK;
 }
 
