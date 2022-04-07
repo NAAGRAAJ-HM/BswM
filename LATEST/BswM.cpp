122d6b85266a8e89a2f9ea851f08b5f0918dbc38
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:25:22 +0000

    Update: AUTOSAR basic software service module BswM

--- LATEST/BswM.cpp
+++ LATEST/BswM.cpp
@@ -101,8 +101,8 @@ FUNC(void, BSWM_CODE) module_BswM::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgBswM;
          }
       }
       IsInitDone = E_OK;
