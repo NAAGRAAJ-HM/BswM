95073d1f4f1e7fd3080c4ba121e8decfae759c13
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:58 +0000

    Update: AUTOSAR basic software service module BswM

--- LATEST/BswM.cpp
+++ LATEST/BswM.cpp
@@ -35,6 +35,13 @@ class module_BswM:
       public abstract_module
    ,  public class_BswM_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_BswM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
