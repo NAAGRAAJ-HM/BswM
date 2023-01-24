e77aded16e8c5011d7a8924c5aab1dbc8ec06764
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:20 +0100

    Update: AUTOSAR basic software service module BswM

---
+++ main_R2023/infServiceBswM_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceBswM_ServiceNvM.hpp                                     */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICEBSWM_AR_RELEASE_VERSION_MAJOR                              4
+#define CONSTSERVICEBSWM_AR_RELEASE_VERSION_MINOR                              3
+
+#define Const_DefaultInit_ConstServiceBswM                                                                               \
+   ,  {CONSTSERVICEBSWM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICEBSWM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
