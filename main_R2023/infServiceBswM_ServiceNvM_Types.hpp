6f47f2ddce65905be3ca08a1ea9f8134d3fb724b
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:30:22 +0100

    Update: AUTOSAR basic software service module BswM

--- main_R2023/infServiceBswM_ServiceNvM_Types.hpp
+++ main_R2023/infServiceBswM_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceBswM_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceBswM_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
