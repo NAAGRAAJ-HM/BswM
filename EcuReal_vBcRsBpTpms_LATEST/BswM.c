da1a5fd8d3fcc3a875304125c0bdbceb7e2b15e4
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2023-11-20 08:21:15 +0100

    AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_LATEST - SwcServiceBswM

---
+++ EcuReal_vBcRsBpTpms_LATEST/BswM.c
@@ -0,0 +1,37 @@
+#include "Std_Types.hpp"
+
+#include "CfgSwcServiceBswM.hpp"
+#include "ComStack_Types.hpp"
+#include "CanSM_BswM.hpp"
+
+CONST(Type_CfgSwcServiceBswM_st, BSWM_CONST) CfgSwcServiceBswM_cst = {
+      NULL_PTR
+   ,  NULL_PTR
+};
+
+void BswM_CanSM_CurrentState(
+      uint8                      ucComM_channelId
+   ,  CanSM_BswMCurrentStateType eCanSM_BswM_Mode
+){
+}
+
+void BswM_Init(
+   const Type_CfgSwcServiceBswM_st* tBswmConfig
+){
+}
+
+void BswM_Deinit(void){
+}
+
+void BswM_EcuM_CurrentWakeup(
+      uint32 EcuM_Lok_dataExpiredWakeupEvents_u32
+   ,  uint8  ucWupStatusMode
+){
+}
+
+void BswM_ComM_CurrentMode(
+      NetworkHandleType ChannelIndex
+   ,  ComM_ModeType     channelMode_e
+){
+}
+
