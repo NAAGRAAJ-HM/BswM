44f6d551502929f6cd5be2e4b5ba5034f025f24f
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2024-02-15 11:42:56 +0100

    Daily back-up

--- vBcRsB_TARGET/b_git_push.sh
+++ vBcRsB_TARGET/b_git_push.sh
@@ -1,4 +1,4 @@
 git add .
 git status
-git commit -m "AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_TARGET - SwcServiceBswM"
+git commit -m "AUTOSAR Migration Activities - rEcu_vBcRsBpTpms_TARGET - SwcServiceBswM"
 git push
